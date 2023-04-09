d06f7568b5f7f1116a57e717d0d3d6a9ac683ef4
URVI12345 <23urviguptaxia@gmail.com>
2023-04-09 23:34:36 +0530

    not complete

--- task2_client.cpp
+++ task2_client.cpp
@@ -2,17 +2,12 @@
 #include "beginner_tutorials/task2.h"
 #include <cstdlib>
 #include "std_msgs/Int64.h"
-#include <xmlrpcpp/XmlRpc.h>
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "disp_client");
 	ros::NodeHandle n;
-	YAML::Node config = YAML::LoadFile("setpoint.yaml");
 	ros::ServiceClient client = n.serviceClient<beginner_tutorials::task2>("task2_server");
-	XmlRpc::XmlRpcServer xmlrpcServer;
-        xmlrpcServer.bindAndListen("my_rpc_server", 8080);
-        xmlrpcServer.bind("my_function", &myXmlRpcFunction);
 	beginner_tutorials::task2 srv;
 	for (const auto& coord : config) {
     long int x = coord["x"].as<long int>();
